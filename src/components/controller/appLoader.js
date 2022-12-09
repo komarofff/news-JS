a82aca894b2a59ab8931b7ac042736bcdcc314a3
Anatoliy Komarov <admin@komaroff.biz>
2022-12-09 09:37:14 +0300

    feat: add eslint and ts for project

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd433e14323954f33beb3535eb246919e', // получите свой ключ https://newsapi.org/
         });
     }
 }
